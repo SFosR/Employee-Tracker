3144b36477d5a6226ebef14e8ef0e94f9fbfd93b
Sheri Foster <87589967+SFosR@users.noreply.github.com>
2021-10-20 13:11:53 -0400

    added my schema and seeds files

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE employess_db;
+
+USE employess_db
+
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+--Below when I call the table "role" it makes it blue--
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30)
+    salary DECIMAL
+    department_id INT
+);
+CREATE TABLE employee (
+    id INT PRIMARY KEY
+    first_name VARCHAR(30)
+    last_name VARCHAR(30)
+    role_id INT
+    manager_id INT NULL
+);
+
