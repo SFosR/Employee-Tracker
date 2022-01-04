f80fd6a5115004a3fa16b27982ee49186ab27310
Sheri Foster <fostr.sheri@gmail.com>
2022-01-04 10:29:49 -0500

    "updated README"

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Employee-Tracker
-A CMS that allows employers to view and manage the departments, roles and employees in their company. Being able to quickly view employess, roles and salaries, companies will be able to better plan their business.
+This employee management system allows employers to view and manage their departments, roles and employees in their company. Being able to quickly & easily view employees, roles and salaries, make additiion and updates, will give companies the ability to better plan and organize their business.
+
+## Video Demo
+### Technologies Used
+* Node.js
+* Inquirer
+* MySQL
+
+#### Created By:
+Sheri Foster  fostr.sheri@gmail.com  GitHub.com/SFosR
 
 
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer");
-const logo = require("asciiart-logo");
+// const logo = require("asciiart-logo");
 const mysql = require("mysql2");
-const { createQuery } = require("mysql2/typings/mysql/lib/Connection");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
@@ -17,12 +16,12 @@ connection.connect((err) => {
   mainMenu();
 });
 
-function init() {
-  const logoText = logo({ name: "Employee Manager"}).render();
-  console.log(logoText);
+// function init() {
+//   const logoText = logo({ name: "Employee Manager"}).render();
+//   console.log(logoText);
 
-  loadmainMenu();
-}
+//   loadmainMenu();
+// }
 
 function mainMenu () {
   inquirer
@@ -42,14 +41,14 @@ function mainMenu () {
         ]
 
       }]).then((answer) => {
-        switch (answer.choice){
+        switch (answer.likeToDo){
 
         case "View all departments?":
           viewAllDepartments();
           break;
   
         case "View all roles?":
-          viewAllPositions();
+          viewAllRoles();
           break;
 
         case "View all employees?":
@@ -68,9 +67,13 @@ function mainMenu () {
           addEmployee();
           break;
 
+        case "Update an employee role?":
+            updateEmployee();
+            break;  
+
         case "Quit":
           connection.end();
-          break;
+          break; 
         }
      })
   };
@@ -78,34 +81,37 @@ function mainMenu () {
 
 function viewAllDepartments () {
      console.log("Viewing all departments");
-     connection.query("SELECT * FROM departments");
-        if (err) throw err;
-        console.table(res);
-        init();
-      };   
+     connection.query("SELECT * FROM departments", (err, res)=>{
+      if (err) throw err;
+      console.table(res);
+      mainMenu();
+     });
+};   
   
-
-
 function viewAllEmployees () {
   console.log("Viewing all employees");
-  connection.query("SELECT * FROM employee");  
-     if (err) throw err;
+  connection.query("SELECT * FROM employee", (err, res)=>{
+    if (err) throw err;
      console.table(res);
-     init();
+     mainMenu();
+  });  
+     
    }; 
 
    function viewAllRoles () {
     console.log("Viewing all roles");
-    connection.query("SELECT * FROM role");
-       if (err) throw err;
+    connection.query("SELECT * FROM role", (err, res)=>{
+      if (err) throw err;
        console.table(res);
-       init();
+       mainMenu();
+    });
+       
      }; 
 
 const addDepartment = () => {
   inquirer.prompt([{
     type: 'input',
-    name: 'newDepartment',
+    name: 'name',
     message: 'What department would you like to add?',
     confirm: newDepartment => {
       if (newDepartment) {
@@ -116,13 +122,13 @@ const addDepartment = () => {
       }
     }
   }]).then((answer) => {
-        connection.query('INSERT INTO department (department_name) VALUES (?)', answer.department, (err, result) => {
+        connection.query('INSERT INTO departments SET ?', answer, (err, result) => {
           if (err) throw err;
-          console.log(answer.newDepartment + ' has been successfully.');
+          console.log(answer.newDepartment + ' has been successful.');
           mainMenu();
         })
   })
-},
+};
 
 const addRole = () => {
   inquirer.prompt([
@@ -152,10 +158,11 @@ const addRole = () => {
       }
   }
 },
-{ type: 'number',
+{ 
+  type: 'number',
   name: 'newDepartmentId',
   message: 'What is the new department Id?',
-  confirm: new DepartmentId => {
+  confirm: newDepartmentId => {
     if (newDepartmentId) {
         return true;
     } else {
@@ -163,14 +170,121 @@ const addRole = () => {
       return false;
     }   
   }
-}
+},
   ]).then((answer) => {
-    connection.query('INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)', [answer.newRole, answer.newSalary, answer.newDepartmentId], (err, result) => {
+    connection.query('INSERT INTO role (title, salary, departments_id) VALUES (?,?,?)', [answer.newRole, answer.newSalary, answer.newDepartmentId], (err, result) => {
         if (err) throw err;
         console.log(answer.newRole + ' has been added successfully.');
+        mainMenu();
     })
   })
-}
+};
+
+const addEmployee = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'newFirstName',
+      message: 'What is the first name of the employee?',
+      confirm: newFirstName => {
+        if (newFirstName) {
+          return true;
+        } else {
+            console.log('Please enter a first name')
+            return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'newLastName',
+      message: 'What is the last name of the employee?',
+      confirm: newLastName => {
+        if (newLastName) {
+          return true;
+        } else {
+          console.log('Please enter a last name')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'number',
+      name: 'newRoleId',
+      message: 'What is the new employees role Id?',
+      confirm: newRoleId => {
+        if (newRoleId) {
+          return true;
+        } else {
+          console.log('Please enter a role Id')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'number',
+      name: 'newManagerId',
+      message: `What is the new employee's manager's Id?`,
+      confirm: newManagerId => {
+        if (newManagerId) {
+          return true;
+        } else {
+          console.log('Please enter a manager Id')
+          return false;
+        }
+      }
+    }
+   
+  ]).then((answer) => {
+    connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)', [answer.newFirstName, answer.newLastName, answer.newRoleId, answer.newManagerId], (err, result) => {
+      if (err) throw err
+        console.log(answer.newFirstName +''+ answer.newLastName +' has been successfully added.');
+        mainMenu();
+    })
+  })
+};
+
+const updateEmployee = ()=> {
+    console.log('update')
+  inquirer.prompt([
+    {
+      type: 'number',
+      name: 'newUpdateId',
+      message: `What is the employee's Id?`,
+      confirm: newUpdateId => {
+        if (newUpdateId) {
+          return true;
+        } else {
+          console.log('Please enter an employee Id')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'number',
+      name: 'newRoleId',
+      message: 'What is the new role Id?',
+      confirm: newRoleId => {
+        if (newRoleId) {
+          return true;
+        } else {
+          console.log('Please enter a role Id')
+          return false;
+        }
+      }
+    }
+]).then((answer) => {
+  connection.query('UPDATE employee SET role_id = ? WHERE id = ?', [answer.newRoleId, answer.newUpdateId], (err, result) => {
+    if (err) throw err
+      console.log(answer.newFirstName +''+ answer.newLastName +' has been successfully added.');
+      mainMenu();
+  })
+})
+};
+
+
+  
+
 
     
 
